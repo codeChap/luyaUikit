1bf3173fe797ffc5971f76932f7b9e0c18125f6e
CodeChap <hello@codechap.com>
2022-09-16 09:27:22 +0200

    Temp vid

--- src/views/blocks/VideoBlock.php
+++ src/views/blocks/VideoBlock.php
@@ -5,5 +5,5 @@ $link = $this->varValue('link', '');
 ?>
 <div class="uk-inline uk-margin uk-flex-center uk-flex-middle">
     <!-- <span class="uk-link uk-text-decoration-none uk-icon-button uk-padding uk-h1 uk-position-center" uk-toggle="target: +">‣</span>-->
-    <video src="<?= $link; ?>" width="1920" height="1080" playsinline uk-video></video>
+    <video src="<?= $link; ?>" width="1920" height="1080" playsinline loop uk-video></video>
 </div>
